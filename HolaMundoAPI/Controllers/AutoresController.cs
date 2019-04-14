11b38c1bc452997a4c45da8d428c9b1cc4d01e56
Paul Cortes <paulkerg@gmail.com>
2019-04-14 11:39:16 -0500

    primer Commit

---
+++ HolaMundoAPI/Controllers/AutoresController.cs
@@ -0,0 +1,48 @@
+﻿using HolaMundoAPI.Context;
+using HolaMundoAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolaMundoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController: ControllerBase 
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObtenerAutor")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(x => x.Id == id);
+            if(autor== null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            context.Autores.Add(autor);
+            context.SaveChanges();
+            return new CreatedAtRouteResult("ObtenerAutor", new  { id = autor.Id }, autor);
+        }
+
+    }
+}
---
+++ HolaMundoAPI/Migrations/20190412205013_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolaMundoAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Nombre = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
